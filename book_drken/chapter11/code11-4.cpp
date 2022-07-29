e80b4dc9c5b6cecf244d2092a7f2c57f45fb88df
hoshinon <yuuki.hoshino.76@gmail.com>
2022-07-29 22:15:37 +0900

    code11.4の写経およびテスト(Union-Find)

---
+++ book_drken/chapter11/code11-4.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// code11.4の写経
+
+struct UnionFind
+{
+	vector<int> parent;  // parent[x]:xの親の番号 xが根であれば-1
+	vector<int> siz;  // siz[x]:xの属するグループの頂点数
+
+	// 頂点数nで初期化
+	UnionFind(int n) : parent(n, -1), siz(n, 1) {}
+
+	// 根を求める
+	int root(int x)
+	{
+		if(parent[x] == -1) return x;  // xが根であれば、xを返す
+		else
+		{
+			parent[x] = root(parent[x]);
+			return parent[x];
+		}
+	}
+
+	// xとyが同じグループに属するか
+	bool issame(int x, int y)
+	{
+		return root(x) == root(y);  // 根が同一なら同じグループ
+	}
+
+	// xを含むグループとyを含むグループを併合する
+	bool unite(int x, int y)
+	{
+		// x,yをそれぞれ根まで移動させる
+		x = root(x);
+		y = root(y);
+
+		// すでに同じグループであれば何もしない
+		if(x == y) return false;
+
+		// 頂点数が少ない方が子になるようにする
+		if(siz[x] < siz[y])
+		{
+			// xをyの子にする
+			parent[x] = y;
+			siz[y] += siz[x];
+		}
+		else
+		{
+			// yをxの子にする
+			parent[y] = x;
+			siz[x] += siz[y];
+		}
+		return true;
+	}
+
+	// xを含むグループの頂点数
+	int size(int x)
+	{
+		return siz[root(x)];
+	}
+};
+
+int main(void)
+{
+	int i;
+	int N, M;  // 頂点数と辺数
+	cin >> N >> M;
+
+	UnionFind uf(N);
+
+	// 頂点番号は0-indexedとする
+	for(i = 0; i < M; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		uf.unite(a, b);  // 辺で繋がる頂点のグループを併合する
+	}
+
+	int answer = 0;
+	for(i = 0; i < N; i++)
+	{
+		if(uf.root(i) == i)  answer++;
+	}
+	cout << answer << endl;
+
+	return 0;
+}
---
+++ book_drken/chapter11/test/problem11-4-test.bat
@@ -0,0 +1,12 @@
+@echo off
+echo [test1]Excepted:3
+..\code11-4.exe < problem11-4-test1.txt
+echo.
+echo [test2]Excepted:3
+..\code11-4.exe < problem11-4-test2.txt
+echo.
+echo [test3]Excepted:5
+..\code11-4.exe < problem11-4-test3.txt
+echo.
+echo [test4]Excepted:1
+..\code11-4.exe < problem11-4-test4.txt
