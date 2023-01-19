134166507bc04eadb68c2a6d642b70b51c8c17d1
hoshinon <yuuki.hoshino.76@gmail.com>
2023-01-19 22:17:47 +0900

    「過去問精選 100問」の問題5(全探索：工夫して通り数を減らす全列挙)
    ABC095-C.cppは当時解いてプッシュしたものを書きなおした。
    実装方針は同じ。

--- AtCoder/ABC001-100/ABC095-C.cpp
+++ AtCoder/ABC001-100/ABC095-C.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
+#include <iomanip>
 using namespace std;
+typedef long long ll;
+const ll INF64 = 1LL << 60;
+const int INF32 = 0x3FFFFFFF;  // =(2^30)-1 10^9より大きく、かつ2倍しても負にならない数
 
-// ABC095 https://atcoder.jp/contests/abc095/
+// 「分野別 初中級者が解くべき過去問精選 100問」の問題5より
+// https://qiita.com/e869120/items/eb50fdaece12be418faa
+// 
+// https://atcoder.jp/contests/abc095/tasks/arc096_a
+
+/*
+ * Aピザ/Bピザの購入枚数について全探索するとO(N^2)となりTLE.
+ * ここではABピザの枚数について全探索する。
+ * ABピザの最小枚数は0枚、最大は max(X, Y)*2 枚となる。
+ * 
+ * ABピザを買う枚数を決めたら、そこからAピザBピザを買う枚数が決まる。
+ * これで購入代金が計算できるので、その最小値を求めればよい。
+ * 
+ * 計算量は、N=max(X,Y)としてO(N).
+ */
 
 int main(void)
 {
-	/*
-	 * ABピザを「一切使わない」から「AピザBピザに全て使う」まで全探索する。
-	 * ABピザをi枚買うとき、
-	 *   Aピザ：X-i/2 枚
-	 *   Bピザ：Y-i/2 枚
-	 * となる。（ただし最小0でクリップ）
-	 */
-	long long A, B, C, X, Y;
+	int A, B, C, X, Y;
 	cin >> A >> B >> C >> X >> Y;
-	long long i;
 	
-	long long answer = 10000000000;  // 十分大きい数
-	for(i = 0; i <= max(X, Y)*2; i+=2)  // ABピザの枚数を、「一切使わない」から「AピザBピザに全て使う」までループ
+	int ab_max = max(X, Y)*2;
+	int answer = INF32;
+	for(int num_ab = 0; num_ab <= ab_max; num_ab += 2)  // ABピザの枚数について全探索
 	{
-		long long num_a, num_b;
-		num_a = max(X-i/2, 0LL);
-		num_b = max(Y-i/2, 0LL);
-		auto tmp = num_a * A + num_b * B + i * C;
-		answer = min(answer, tmp);
+		int num_a, num_b;  // AピザおよびBピザを買う枚数
+		num_a = max(X-num_ab/2, 0);  // 最小0枚でクリップ
+		num_b = max(Y-num_ab/2, 0);
+		int price = num_a * A + num_b * B + num_ab * C;
+		answer = min(answer, price);
 	}
 
 	cout << answer << endl;
