6d621f2cf397a5a927d1a8c13a34dd1ea86f6d22
hoshinon <yuuki.hoshino.76@gmail.com>
2023-05-02 17:26:53 +0900

    ABC153 F問題(遅延セグメント木)

--- AtCoder/ABC100-200/ABC153-F-2.cpp
+++ AtCoder/ABC100-200/ABC153-F-2.cpp
@@ -1,47 +1,28 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <limits>  // numeric_limits
-#include <cassert>
+#include <cmath>
+#include <iomanip>
 using namespace std;
 typedef long long ll;
 const ll INF64 = 1LL << 60;
 const int INF32 = 0x3FFFFFFF;  // =(2^30)-1 10^9より大きく、かつ2倍しても負にならない数
 #define YesNo(T) cout << ((T) ? "Yes" : "No") << endl;  // T:bool
 
-// 遅延セグメント木のメモや実装
+// ABC153 https://atcoder.jp/contests/abc153
 
 /*
- * [ざっくり概要]
- * ・任意の区間の値を更新する(区間更新), 一律加算する(区間加算)
- * ・任意の区間における最小値や合計値を取得する
- * といった処理をO(logN)でできるデータ構造。
+ * 遅延セグメント木で解く。
  * 
- * LazySegmentTree_RAQ_RSQ:
- * Range Add Query(RAQ)およびRange Sum Query(RSQ)をセグメント木で実装したもの。
- *   以下の操作をO(logN)で処理できる。
- *   (1)Add(a, b, x) : 区間[a,b)の要素にxを加算する
- *   (2)GetSum(a, b) : 区間[a,b)にある要素の総和を返す
- *   0-indexed, および半開区間で処理する。
+ * 使うライブラリをBITから遅延セグ木に変更したのみで、
+ * 考え方はABC153-F.cppと同様。
+ * (※)ライブラリの都合上、1-indexedから0-indexedに実装を変更している
  * 
- * [Tips]
- * ・木の最下段のノード数は、問題文にて指定されるsize以上の2のべき乗。
- *   これをNとすると、最下段のノード数はN, それより上の段のノードは全部でN-1.
- *   よって木全体で必要なノード数は 2N-1 となる。
- * ・要素xをnode[]の添字番号に変換する場合：N-1を加算する
- * ・親から子へ行く場合、 k -> 2k+1, 2k+2
- * ・子から親へ行く場合、 k -> (k-1)/2  (切り捨て)
- * 
- * [参考資料]
- *   https://tsutaj.hatenablog.com/entry/2017/03/30/224339
- *   https://algo-logic.info/segment-tree/
- * 
- * [関連する問題]
- * AOJ DSL_2_G https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_G&lang=ja
- * ABC035-C
- * square869120Contest#2-H https://atcoder.jp/contests/s8pc-2
- * JOI 2008年春合宿 - Typhoon
- * ABC153-F
+ * ちなみにAtCoder上の実行時間は
+ *   BIT：160ms
+ *   遅延セグ木：281ms
+ * であった。
+ * 操作が同じO(logN)であってもBITの方が定数倍が軽い、ということが分かる。
  */
 
 // Range Add Query(RAQ)およびRange Sum Query(RSQ)の実装
@@ -151,49 +132,47 @@ public:
 	}
 };
 
-
-void Test(void)
-{
-	LazySegmentTree_RAQ_RSQ<int> seg(6);  // [0,6)
-	seg.Add(0, 2, 10);  // {10, 10,  0,  0, 0, 0}
-	seg.Add(1, 4, 20);  // {10, 30, 20, 20, 0, 0}
-	assert(seg.GetSum(0, 3) == 60);
-	assert(seg.GetSum(0, 6) == 80);
-	seg.Add(0, 6, 5);  //  {15, 35, 25, 25, 5, 5}
-	assert(seg.GetSum(4, 6) == 10);
-	assert(seg.GetSum(0, 6) == 110);
-}
-
 int main(void)
 {
-	Test();
-
-	// 以下は AOJ DSL_2_G のもの
-	// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_G&lang=ja
-	int n, q;
-	cin >> n >> q;
-	LazySegmentTree_RAQ_RSQ<ll> seg(n);
-
-	int c, s, t, x;
-	while(q > 0)
+	// 0-indexed
+	ll i;
+	ll N, D, A;
+	cin >> N >> D >> A;
+	LazySegmentTree_RAQ_RSQ<ll> seg(N);  // 要素iのモンスターのHP 座標は別配列x[]にて管理
+	vector<pair<ll, ll>> mons;  // {座標X, HP} モンスターを座標順にソートする
+	vector<ll> x(N);
+	for(i = 0; i < N; i++)
 	{
-		cin >> c;
-		if(c == 0)  // add
-		{
-			cin >> s >> t >> x;
-			s--; t--;  // 0-indexedへの変換
-			t++;  // 閉区間から半開区間への変換
-			seg.Add(s, t, x);
-		}
-		else  // getSum
-		{
-			cin >> s >> t;
-			s--; t--;  // 0-indexedへの変換
-			t++;  // 閉区間から半開区間への変換
-			cout << seg.GetSum(s, t) << endl;
-		}
-		q--;
+		ll x, h;
+		cin >> x >> h;
+		mons.push_back({x, h});
 	}
+	sort(mons.begin(), mons.end());  // モンスターの情報を座標順にソート
+	for(i = 0; i < N; i++)
+	{
+		seg.Add(i, i+1, mons[i].second);
+		x[i] = mons[i].first;
+	}
+	// 以降、座標x[]は昇順ソート済み
+
+	// モンスター1から順に、HPが残っている敵iを見つける。
+	// なるべく右側に爆弾をなげるため、 x[敵i]+2*D <= x[敵idx] となる最大のidxを見つける。
+	// 爆弾を投げる回数は、 HP[i]/A の切り上げとなる。
+	ll answer = 0;
+	for(i = 0; i < N; i++)  // モンスター:i
+	{
+		if(seg.GetSum(i, i+1) <= 0) continue;  // 撃破済み
+
+		// x[i]*2*Dのupper_bound(), の1つ手前
+		auto idx = upper_bound(x.begin(), x.end(), x[i] + D*2) - x.begin();
+		idx--;
+
+		// [i,idx]についてダメージを与えるので、セグ木には[i,idx+1)で指定する
+		ll count = (seg.GetSum(i,i+1)+(A-1)) / A;  // モンスターiを撃破するために使う爆弾の数 HP/Aを切り上げ
+		seg.Add(i, idx+1, -(A*count));
+		answer += count;
+	}
+	cout << answer << endl;
 
 	return 0;
 }
