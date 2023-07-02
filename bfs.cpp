898849c0c41303b7b9f2a2cdba92997f841b91bd
hoshinon <yuuki.hoshino.76@gmail.com>
2023-07-02 23:06:44 +0900

    グリッドグラフをライブラリ化

--- bfs.cpp
+++ bfs.cpp
@@ -1,3 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+typedef long long ll;
+const ll INF64 = 1LL << 60;
+const int INF32 = 0x3FFFFFFF;  // =(2^30)-1 10^9より大きく、かつ2倍しても負にならない数
 
 // 幅優先探索のメモ
 
@@ -42,3 +50,59 @@
  *   https://drken1215.hatenablog.com/entry/2021/07/30/024800
  * 
  */
+
+vector<string> mp;
+
+// 高さH, 幅Wのグリッドグラフについて、(y,x)=(sy,sx)を起点にBFS.
+// distは要素数[H][W]で、未探索部分はINF32で埋めていること。
+// 探索結果として、distに(sy,sx)を起点(=0)とした距離を格納する。
+void bfs_grid_graph(int H, int W, int sy, int sx, vector<vector<int>> &dist)
+{
+	queue<pair<int,int>> que;
+
+	dist[sy][sx] = 0;
+	que.push({sy, sx});
+
+	const int DIR = 4;
+	int dx[DIR] = {1, -1, 0, 0};
+	int dy[DIR] = {0, 0, 1, -1};
+	while(!que.empty())
+	{
+		int y = que.front().first;
+		int x = que.front().second;
+		que.pop();
+		for(int i = 0; i < DIR; i++)
+		{
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if(ny < 0 || ny >= H || nx < 0 || nx >= W) continue;
+			if(dist[ny][nx] != INF32) continue;
+
+			// 壁など 問題によって書き換える
+			if(mp[ny][nx] == '#') continue;
+
+			dist[ny][nx] = dist[y][x] + 1;
+			que.push({ny, nx});
+		}	
+	}
+	return;
+}
+
+int main(void)
+{
+	/*
+	連結成分の数を求めたい場合、以下の実装イメージとなる。
+	for(y = 0; y < H; y++)
+	{
+		for(x = 0; x < W; x++)
+		{
+			if(mp[y][x] = (通れるマス) && dist[y][x] == INF32)  // 通れるマスで、かつBFS未実施のマス
+			{
+				bfs_grid_graph(H, W, y, x, dist);
+				ans++;
+			}
+		}
+	}
+	*/
+	return 0;
+}
