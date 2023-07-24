cbecb3c5376248bad65c7d08bf1fe30bca03e5c1
hoshinon <yuuki.hoshino.76@gmail.com>
2023-07-24 18:05:33 +0900

    「過去問精選 100問」の問題85(UnionFind)

---
+++ AtCoder/past_questions_100/81-100/past-questions-85.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+typedef long long ll;
+const ll INF64 = 1LL << 60;
+const int INF32 = 0x3FFFFFFF;  // =(2^30)-1 10^9より大きく、かつ2倍しても負にならない数
+#define YesNo(T) cout << ((T) ? "Yes" : "No") << endl;  // T:bool
+
+// 「分野別 初中級者が解くべき過去問精選 100問」の問題85
+// https://qiita.com/e869120/items/eb50fdaece12be418faa
+// 
+// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A&lang=ja
+
+// ライブラリ化したUnionFindを使えばOK.
+
+struct UnionFind
+{
+	vector<int> parent;  // parent[x]:xの親の番号 xが根であれば-1
+	vector<int> siz;  // siz[x]:xの属するグループの頂点数
+
+	// 頂点数nで初期化
+	UnionFind(int n) : parent(n, -1), siz(n, 1) {}
+
+	// 根を求める
+	int root(int x)
+	{
+		if(parent[x] == -1) return x;  // xが根であれば、xを返す
+		else
+		{
+			parent[x] = root(parent[x]);
+			return parent[x];
+		}
+	}
+
+	// xとyが同じグループに属するか
+	bool issame(int x, int y)
+	{
+		return root(x) == root(y);  // 根が同一なら同じグループ
+	}
+
+	// xを含むグループとyを含むグループを併合する
+	bool unite(int x, int y)
+	{
+		// x,yをそれぞれ根まで移動させる
+		x = root(x);
+		y = root(y);
+
+		// すでに同じグループであれば何もしない
+		if(x == y) return false;
+
+		// 頂点数が少ない方が子になるようにする
+		if(siz[x] < siz[y])
+		{
+			// xをyの子にする
+			parent[x] = y;
+			siz[y] += siz[x];
+		}
+		else
+		{
+			// yをxの子にする
+			parent[y] = x;
+			siz[x] += siz[y];
+		}
+		return true;
+	}
+
+	// xを含むグループの頂点数
+	int size(int x)
+	{
+		return siz[root(x)];
+	}
+};
+
+int main(void)
+{
+	int n, q; cin >> n >> q;
+	UnionFind uf(n);
+
+	while(q > 0)
+	{
+		int com, x, y; cin >> com >> x >> y;
+		if(com == 0)
+		{
+			uf.unite(x, y);
+		}
+		else
+		{
+			cout << (uf.issame(x, y) ? 1 : 0) << endl;
+		}
+		q--;
+	}
+
+	return 0;
+}
