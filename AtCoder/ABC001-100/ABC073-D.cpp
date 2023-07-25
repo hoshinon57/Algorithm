e1e5846ee77159ff679f0d4dd8a638d6b6061bc5
hoshinon <yuuki.hoshino.76@gmail.com>
2023-07-25 23:40:09 +0900

    ABC073 D問題(ワーシャルフロイド、順列全探索)

--- AtCoder/ABC001-100/ABC073-D.cpp
+++ AtCoder/ABC001-100/ABC073-D.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
+#include <iomanip>
 using namespace std;
 typedef long long ll;
 const ll INF64 = 1LL << 60;
 const int INF32 = 0x3FFFFFFF;  // =(2^30)-1 10^9より大きく、かつ2倍しても負にならない数
 #define YesNo(T) cout << ((T) ? "Yes" : "No") << endl;  // T:bool
 
-// ワーシャルフロイド法のメモ、ライブラリ
-// 英語表記では Floyd–Warshall が一般的のようだが、
-// 「ワーシャルフロイド」で覚えており、検索性のため warshall-floyd.cpp としている
+// ABC073 https://atcoder.jp/contests/abc073
 
 /*
- * [関連する問題]
- * AOJ GRL_1_C https://onlinejudge.u-aizu.ac.jp/problems/GRL_1_C
- * ABC073-D
+ * ワーシャルフロイドと順列全探索で解く。
+ * 
+ * まずは全町間の距離をワーシャルフロイド法で求める。
+ * r1,r2,...,rRへの訪れ方は、next_permutation()を用いて全パターン探索する。
+ * 各訪れ方ごとに移動距離を計算し、その最小値が答となる。
+ * 
+ * 計算量はワーシャルフロイド部分がO(N^3), 順列全探索の部分がO(R!)であり、
+ * 全体でO(N^3 + R!)となる。
+ * N,Rどちらも小さい値のため、十分に間に合う。
  */
 
 // ワーシャルフロイド法を用いて全点対間最短経路を解く。
@@ -52,45 +58,34 @@ bool Warshall_Floyd(vector<vector<ll>> &dist)
 	return exist_negative_cycle;
 }
 
-void solve(void)
-{
-	// 以下はAOJのGRL_1_Cを解く内容
-	// https://onlinejudge.u-aizu.ac.jp/problems/GRL_1_C
-
-	int i, j;
-	int V, E; cin >> V >> E;
-	vector<vector<ll>> dist(V, vector<ll>(V, INF64));
-	for(i = 0; i < V; i++) dist[i][i] = 0;
-	for(i = 0; i < E; i++)
-	{
-		ll s, t, d; cin >> s >> t >> d;
-		dist[s][t] = d;
-	}
-	bool negative_cycle = Warshall_Floyd(dist);
-
-	if(negative_cycle)
-	{
-		cout << "NEGATIVE CYCLE" << endl;
-		return;
-	}
-	for(i = 0; i < V; i++)
-	{
-		for(j = 0; j < V; j++)
-		{
-			if(dist[i][j] == INF64)
-				cout << "INF";
-			else
-				cout << dist[i][j];
-			if(j != V-1) cout << " ";
-		}
-		cout << endl;
-	}
-	return;
-}
-
 int main(void)
 {
-	solve();
+	int i;
+	int N, M, R; cin >> N >> M >> R;
+	vector<int> r(R); for(i = 0; i < R; i++) {cin >> r[i]; r[i]--;}
+	sort(r.begin(), r.end());  // 念のためソートしておく
+
+	vector<vector<ll>> dist(N, vector<ll>(N, INF64));
+	for(i = 0; i < M; i++)
+	{
+		int a, b, c; cin >> a >> b >> c;
+		a--; b--;
+		dist[a][b] = dist[b][a] = c;
+	}
+	for(i = 0; i < N; i++) dist[i][i] = 0;
+	Warshall_Floyd(dist);
+
+	ll ans = INF64;
+	do
+	{
+		ll sum = 0;
+		for(i = 0; i < R-1; i++)
+		{
+			sum += dist[r[i]][r[i+1]];
+		}
+		ans = min(ans, sum);
+	} while (next_permutation(r.begin(), r.end()));
+	cout << ans << endl;
 
 	return 0;
 }
