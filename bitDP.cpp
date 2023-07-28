58c2780c0c8025a73175d77e9c2f61069ed82863
hoshinon <yuuki.hoshino.76@gmail.com>
2023-07-28 22:55:59 +0900

    bitDPのライブラリを作り直し
    「スタート地点を頂点0」という制約を追加した。
    これにより、呼び出し下での処理が多少簡易になる。

--- bitDP.cpp
+++ bitDP.cpp
@@ -1,98 +1,105 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cmath>
-#include <iomanip>
 using namespace std;
 typedef long long ll;
 const ll INF64 = 1LL << 60;
-const int INF32 = 1 << 30;
+const int INF32 = 0x3FFFFFFF;  // =(2^30)-1 10^9より大きく、かつ2倍しても負にならない数
+#define YesNo(T) cout << ((T) ? "Yes" : "No") << endl;  // T:bool
+
+// bitDPのメモ、ライブラリ
 
 /*
- * [関連する問題]
- * https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_2_A&lang=ja
- * ABC274-E
- */
-
-/*
- * 以下、bitDPの典型問題として、巡回セールスマン問題をO(N*2^N)で解く。
- * 問題文：https://qiita.com/drken/items/7c6ff2aa4d8fce1c9361 の"bit DP"
+ * [参考記事]
+ * https://algo-logic.info/bit-dp/
  * 
  * dp[S][v]を以下のように定める。
  *   Sの各ビットをそれぞれの都市に割り当てて、
  *   1になっているビットに対して好きな順に辿ったときの、最小距離
- *   ただし最後にvに行く、という制約
+ *   ただしスタート地点を0とし、最後にvに行く、という制約
  *   未探索であれば-1
  * 
- * 例えばS=b'1101, v=0であれば、
+ * 例えばS=b'1101, v=2であれば、
  *   頂点0,2,3を好きな順に辿ったときの、最小距離
- *   ただし最後に行くのは頂点0
+ *   ただし最後に行くのは頂点2
  * となる。
  * 
  * dpの遷移式は以下となる。
- *   dp[S][v] = min(dp[S-{v}][i] + dist[i][v])
- *   ※ただしiは、S-{v}に含まれる頂点iそれぞれについて計算する
- * 頂点vを除いた集合の移動距離(終点i) + i->vの距離 という考え方。
+ *   dp[S][v] = min(dp[S-{v}][u] + dist[u][v])
+ *   ※ただしuは、S-{v}に含まれる頂点iそれぞれについて計算する
+ * 頂点vを除いた集合の移動距離(終点u) + u->vの距離 という考え方。
  * 
- * あとは上記を実装した関数func()を用意し、各頂点vに対して
- *   func((全頂点のbitを1にした値), v)
- * の最小値が答となる。
+ * [関連問題]
+ * AOJ DPL_2_A https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_2_A&lang=ja
+ * ABC274-E
  */
 
-const int MAX_N = 20;
-int dist[MAX_N][MAX_N];  // dist[i][j]:i->jへの距離
-int dp[(1<<MAX_N)][MAX_N];
-
-int N;
+const int MAX_N = 15;  // 頂点数の最大
+int dist[MAX_N][MAX_N];  // dist[i][j]:頂点i->jへの距離
+// dp[S][v]:
+//   Sの各ビットをそれぞれの都市に割り当てて、
+//   1になっているビットに対して好きな順に辿ったときの、最小距離
+//   ただし最後にvに行く、という制約
+//   未探索であれば-1
+int dp[1<<MAX_N][MAX_N];
 
 // Sの各ビットをそれぞれの都市に割り当てて、
-// 1になっているビットに対して好きな順に辿ったときの、最小距離
-// ただし最後にvに行く、という制約
-int func(int S, int v)
+// 1になっているビットに対して好きな順に辿ったときの、最小距離を返す
+// ただしスタート地点を0とし、最後にvに行く、という制約
+// N:頂点数
+// 呼び出し元での事前準備：
+//   dist[][]に頂点間の距離を設定する
+//   dp[][]を未探索の-1にする
+int bitDP(int S, int v, int N)
 {
-	// メモ化再帰
-	if(dp[S][v] != -1) return dp[S][v];
+	// メモ化再帰で解く
+	if(dp[S][v] != -1) return dp[S][v];  // メモ化済み
 
-	// Sにて1になっているビットが1つ、つまり最初の都市の場合
-	if(S == (1<<v))
+	if(S == 0)  // ベースケース
 	{
-		dp[S][v] = 0;  // まだ移動していないので、距離は0
+		// 頂点0をスタートとするので、dp[0][0]は0, dp[0][*]はありえないためINF.
+		if(v == 0) dp[S][v] = 0;
+		else dp[S][v] = INF32;
+
 		return dp[S][v];
 	}
 
-	// 最後にvに行くという制約より、vに行く前のSの状態を取得する
-	int S_prev = S & (~(1<<v));  // vに相当するビットのみ落とす
+	if(((S >> v) & 1) == 0) {dp[S][v] = INF32; return dp[S][v];}  // Sに頂点vが含まれていない
+	int S_prev = S & (~(1<<v));  // vに相当するビットを落とす
 
-	int ret = INF32;
-	for(int i = 0; i < N; i++)
+	int &d = dp[S][v];
+	d = INF32;
+	for(int u = 0; u < N; u++)
 	{
-		if((S_prev & (1<<i)) == 0) continue;  // 1になっていないビットはスルー
-
-		// dp[S][v]を求めたい
-		// 都市vを除いた集合の移動距離(終点i) + i->vの距離 となる
-
-		// dp[S][v] = dp[S-{v}][i] + dist[i][v]
-		ret = min(ret, func(S_prev, i) + dist[i][v]);
+		// S_prev内の頂点uを決めて、u->vへの遷移を考える
+		if(dist[u][v] == INF32) continue;  // u->vへの辺が無ければスルー
+		
+		// dp[S][v] = dp[S-{v}][u] + dist[u][v]
+		d = min(d, bitDP(S_prev, u, N) + dist[u][v]);
 	}
-	dp[S][v] = ret;
-
-	return dp[S][v];
+	return d;
 }
 
 int main(void)
 {
+	// 以下はAOJ DPL_2_Aを解く内容
+	// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_2_A&lang=ja
 	int i, j;
-	cin >> N;
+	int N, M; cin >> N >> M;
 	for(i = 0; i < N; i++)
 	{
 		for(j = 0; j < N; j++)
 		{
-			cin >> dist[i][j];
+			dist[i][j] = INF32;  // 辺で結ばれていない頂点間は距離をINFにする
 		}
 	}
-
+	for(i = 0; i < M; i++)
+	{
+		int s, t, d; cin >> s >> t >> d;
+		dist[s][t] = d;
+	}
 	// dpテーブル初期化
-	for(i = 0; i < (1<<N); i++)
+	for(i = 0; i < 1<<N; i++)
 	{
 		for(j = 0; j < N; j++)
 		{
@@ -100,12 +107,9 @@ int main(void)
 		}
 	}
 
-	int answer = INF32;
-	for(i = 0; i < N; i++)
-	{
-		answer = min(answer, func((1<<N)-1, i));
-	}
-	cout << answer;
+	int ans = bitDP((1<<N)-1, 0, N);
+	if(ans == INF32) ans = -1;
+	cout << ans << endl;
 
 	return 0;
 }
