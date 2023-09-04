5d90de2ec3456573b6983e2a4403127972889ba6
hoshinon <yuuki.hoshino.76@gmail.com>
2023-09-04 22:39:42 +0900

    recursive_dfs_2()修正、関連問題追加

--- recursive_dfs.cpp
+++ recursive_dfs.cpp
@@ -10,8 +10,10 @@ using namespace std;
 /*
  * [関連する問題]
  * ABC232-C https://atcoder.jp/contests/abc232/tasks/abc232_c
+ * ABC236-D https://atcoder.jp/contests/abc236/tasks/abc236_d (ペアを作る)
  * ABC268-D https://atcoder.jp/contests/abc268/tasks/abc268_d (D問題にしては難易度高めだが、良問だった)
  * ABC310-D https://atcoder.jp/contests/abc310/tasks/abc310_d (これ、コンテスト中に解けなかったのは反省)
+ * ABC318-D https://atcoder.jp/contests/abc318/tasks/abc318_d (ペアを作る＆Nが奇数のケース有, 解けなかったが良問)
  */
 
 const int NUM = 3;
@@ -47,9 +49,10 @@ const int dispNum[NUM2] = {1, 2, 3, 4, 5};
 /*
  * 1,2,3,4,5 を用いた順列を全て列挙する。
  * a[]に要素番号を積み、順列の表示はdispNum[]を使う。
+ * used[i]:要素iをa[]に積んだらtrue. 呼び出し元にて全要素をfalseに初期化しておくこと。
  * next_permutation()というライブラリもあるようだが、使わないで実装してみた。
  */
-void recursive_dfs_2(vector<int> &a)
+void recursive_dfs_2(vector<int> &a, vector<bool> &used)
 {
 	int i;
 	if(a.size() == NUM2)  // 順列を生成したので処理  ★ここを問題に合わせて実装する
@@ -62,20 +65,15 @@ void recursive_dfs_2(vector<int> &a)
 		return;
 	}
 
-	// まだa[]に積んでいないものを探す
-	vector<bool> b(NUM2, false);
-	for(i = 0; i < a.size(); i++)
-	{
-		b[a[i]] = true;
-	}
-
 	// 再帰の処理  ★問題に合わせて再帰の条件を実装する
 	for(i = 0; i < NUM2; i++)
 	{
-		if(b[i]) continue;  // iはa[]に積み済
+		if(used[i]) continue;  // iはa[]に積み済
+		used[i] = true;
 		a.push_back(i);
-		recursive_dfs_2(a);
+		recursive_dfs_2(a, used);
 		a.pop_back();
+		used[i] = false;
 	}
 
 	return;
@@ -87,7 +85,8 @@ int main(void)
 	recursive_dfs(a);
 
 	vector<int> a2;
-	recursive_dfs_2(a);
+	vector<bool> used(NUM2, false);
+	recursive_dfs_2(a, used);
 
 	return 0;
 }
