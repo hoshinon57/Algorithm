36ed3f67531aeab9d0103d71afed768cd641409c
hoshinon <yuuki.hoshino.76@gmail.com>
2023-11-22 18:38:34 +0900

    パスカルの法則を用いたnCkの計算ライブラリを追加

--- combination.cpp
+++ combination.cpp
@@ -68,6 +68,30 @@ ll nCk(ll n, ll k)
 	return x * ((y * z) % MOD) % MOD;  //nCkの計算
 }
 
+// MODではなく正確な値を求めるような問題において、
+// パスカルの法則を用いてnCkを計算するライブラリ
+const int NCK_PASCAL_SIZE = 61;  // nCkのnの最大値 問題文の制約に合わせる
+ll nCk_pascal_memo[NCK_PASCAL_SIZE][NCK_PASCAL_SIZE];
+void nCk_Pascal_Init(void)
+{
+	int i, j;
+	for(i = 0; i < NCK_PASCAL_SIZE; i++)
+	{
+		for(j = 0; j < NCK_PASCAL_SIZE; j++)
+		{
+			nCk_pascal_memo[i][j] = -1;
+		}
+	}
+}
+ll nCk_Pascal(int n, int k)
+{
+	// nCr = (n-1)Cr + (n-1)C(r-1)    ※パスカルの法則
+	if(k == 0) return 1;
+	if(n == k) return 1;
+	if(nCk_pascal_memo[n][k] != -1) return nCk_pascal_memo[n][k];
+	return nCk_pascal_memo[n][k] = nCk_Pascal(n-1, k) + nCk_Pascal(n-1, k-1);
+}
+
 int main(void)
 {
 	nCk_init();
@@ -89,6 +113,13 @@ int main(void)
 	 * 60C30=10^17 近辺までなら計算できる。
 	 * 実装参考：https://atcoder.jp/contests/abc202/submissions/47821692
 	 */
+	// MODを用いない範囲の問題について、パスカルの法則で解く方法。
+	nCk_Pascal_Init();
+	cout << "10C2=" << nCk_Pascal(10, 2) << endl;  // 45
+	cout << "5C5=" << nCk_Pascal(5, 5) << endl;  // 1
+	cout << "3C0=" << nCk_Pascal(3, 0) << endl;  // 1
+	cout << "20C10=" << nCk_Pascal(20, 10) << endl;  // 184756
+	cout << "60C30=" << nCk_Pascal(60, 30) << endl;  // 118264581564861424  この辺りが数値的に上限
 
 	return 0;
 }
