94a3bccaf24878e8d48633fdd47605743ccc35ee
hoshinon <yuuki.hoshino.76@gmail.com>
2023-12-21 22:26:21 +0900

    bit操作追加

--- other_library.cpp
+++ other_library.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <limits>  // numeric_limits
 #include <cassert>
 using namespace std;
+typedef long long ll;
 
 /*
  * その他のライブラリ
  * 
  */
+
+// 以下、bは0-indexed
+bool isbiton(ll x, int b) { return ((x>>b)&1); }  // xのbビット目が立っていればtrue (bは0-indexed)
+void setbit(ll &x, int b) { x |= ((ll)1<<b); }  // xのbビット目を立てる
+void unbit(ll &x, int b) { x &= (~((ll)1<<b)); }  // xのbビット目を落とす
 
 // 要素がN*Nであるaについて、右に90度回転させる
 void rotate(vector<vector<int>> &a)
@@ -25,6 +32,24 @@ void rotate(vector<vector<int>> &a)
 
 int main(void)
 {
+	ll x = 0b1000;
+	assert(!isbiton(x, 4));
+	assert( isbiton(x, 3));
+	assert(!isbiton(x, 2));
+	x = numeric_limits<ll>::max();
+	assert( isbiton(x,  0));
+	assert( isbiton(x, 62));
+	assert(!isbiton(x, 63));
+	x = 0;
+	setbit(x, 1); assert(isbiton(x, 1)); assert(!isbiton(x, 0));
+	setbit(x, 62); assert(isbiton(x, 62));
+	setbit(x, 1);  // 既に立っているビット
+	assert(x == ((ll)1<<62 | (ll)1<<1));
+	unbit(x, 62);
+	unbit(x, 62);  // 既に落ちているビット
+	assert(!isbiton(x, 62));
+	assert(x == ((ll)1<<1));
+
 	vector<vector<int>> a(4, vector<int>(4)), b(4, vector<int>(4));
 	a = {{1,0,0,0},
 	     {1,1,0,0},
