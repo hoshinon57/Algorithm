09e9f383babdb84d52eff0f5540fe76db2acb7de
hoshinon <yuuki.hoshino.76@gmail.com>
2024-02-18 23:16:42 +0900

    ABC223 F問題(セグメント木)

---
+++ AtCoder/ABC201-300/ABC221-230/ABC223-F.cpp
@@ -0,0 +1,233 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <functional>  // function
+#include <limits>  // numeric_limits
+#include <iomanip>
+using namespace std;
+typedef long long ll;
+const ll INF64 = 1LL << 60;
+const int INF32 = 0x3FFFFFFF;  // =(2^30)-1 10^9より大きく、かつ2倍しても負にならない数
+template<class T> inline bool chmin(T &a, T b) { if(a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T &a, T b) { if(a < b) { a = b; return true; } return false; }
+#define YesNo(T) cout << ((T) ? "Yes" : "No") << endl;  // T:bool
+
+// ABC223 https://atcoder.jp/contests/abc223
+
+/*
+ * セグ木で解く。基本は公式解説の通り。
+ *   https://atcoder.jp/contests/abc223/editorial/2774
+ * 
+ * '('を+1, ')'を-1とみなす。
+ * カッコ列が正しいかは「'('と')'の数が同じ」「左から見ていって、常に'('の数>=')'の数となっている」であること。
+ * よってセグ木で、()を左から加算していったときの 総和 と 最小値 を管理すれば解ける。
+ * 
+ * 実装では array<int,2> で [0]を総和、[1]を最小値とした。
+ * 要素a,bの二項演算は
+ *   [0]:a[0]+b[0]
+ *   [1]:min(a[1], a[0]+b[1])
+ * として定義できる。
+ * 単位元は{0,0}.
+ * 
+ * 入れ替えクエリは、単に要素を取得して反対側に設定すればよい。
+ * (よって遅延セグ木を使わずとも、1点更新のセグ木で解ける)
+ * 
+ * [ACまでの思考の流れ]
+ * ・カッコ列が正しいかの判定は典型。
+ * ・入れ替えクエリがあるので、セグ木か遅延セグ木っぽい。
+ * ・カッコ列判定をするための情報をセグ木に乗せればいけそう。
+ */
+
+template <typename T>
+struct SegmentTree
+{
+private:
+	using FX = function<T(T, T)>;  // X*X->X となる関数の型
+	int n;   // 木の最下段の要素数 (コンストラクタで指定したsize以上の、2のべき乗)
+	FX fx;  // モノイドX上での二項演算
+	const T ex;  // モノイドX上の単位元
+	vector<T> node;
+
+public:
+	// 要素数で初期化
+	SegmentTree(int size, FX fx_, T ex_) : fx(fx_), ex(ex_)
+	{
+		// 最下段のノード数は、size以上の2のべき乗 -> nとする
+		// するとセグメント木全体で必要なノード数は 2*n-1 となる
+		n = 1;
+		while(n < size) n *= 2;
+		node.resize(2*n-1, ex);  // 単位元で初期化
+	}
+
+	// 要素xをvalで更新する
+	void Update(int x, T val)
+	{
+		x += n-1;  // node[]の要素番号に変換
+		// 最下段のnodeを更新したら、あとは親へ上って更新していく
+		node[x] = val;
+		while(x > 0)
+		{
+			x = (x-1)/2;  // 親
+			node[x] = fx(node[2*x+1], node[2*x+2]);
+		}
+	}
+
+	// 区間[a,b)にある要素のモノイド積を返す
+	// k:自分がいるnodeのindex
+	// nodeの[l,r)を対象とする
+	T Query(int a, int b, int k = 0, int l = 0, int r = -1)
+	{
+		// r=-1 なら最初の呼び出し
+		if(r < 0) r = n;  // [0,n)を対象とする
+
+		// クエリ[a,b)と対象[l,r)が交わらないので、答に影響しない値(=単位元)を返す
+		if(r <= a || b <= l) return ex;
+
+		// クエリが対象を完全に被覆する
+		if(a <= l && r <= b) return node[k];
+
+		// 左右の子について再帰的に探索
+		T vl = Query(a, b, 2*k+1, l, (l+r)/2);  // 左側
+		T vr = Query(a, b, 2*k+2, (l+r)/2, r);  // 右側
+		return fx(vl, vr);
+	}
+
+	// [注意]
+	// 以下のFind_Leftmostは、x以下のものを探すのであれば(現状のソースコード)、Range Minのfx,exを指定すること。
+	// x以上のものを探したい場合、
+	//   Range "MAX"のfx,exを指定
+	//   関数内を1か所修正：node[k] > x を node[k] < x へ変更
+	// とすること。
+
+	// [a,b)の範囲で、x以下となる最も左側の要素番号を返す
+	// 範囲内にx以下が見つからなければ、b(=範囲外)を返す
+	// k:自分がいるnodeのindex
+	// nodeの[l,r)を対象とする
+	int Find_Leftmost(int a, int b, T x, int k = 0, int l = 0, int r = -1)
+	{
+		// r=-1 なら最初の呼び出し
+		if(r < 0) r = n;  // [0,n)を対象とする
+
+		// 自分の値がxより大きい   もしくは
+		// クエリ[a,b)と対象[l,r)が交わらない
+		if(node[k] > x || (r <= a || b <= l)) return b;  // 自身の右隣を返す
+
+//		if(k >= n-1) return k-(n-1);  // 自分が葉なら、その位置を返す
+		if(k >= n-1) return l;  // 自分が葉なら、その位置を返す
+		// 葉なので、lが位置を表している
+
+		int vl = Find_Leftmost(a, b, x, 2*k+1, l, (l+r)/2);  // 左側
+		if(vl != b)  // 左側に答がある
+		{
+			return vl;
+		}
+		else
+		{
+			return Find_Leftmost(a, b, x, 2*k+2, (l+r)/2, r);  // 右側
+		}
+	}
+
+	// [注意]Find_Leftmost()側のコメント参照
+
+	// [a,b)の範囲で、x以下となる最も右側の要素番号を返す
+	// 範囲内にx以下が見つからなければ、a-1(=範囲外)を返す
+	// k:自分がいるnodeのindex
+	// nodeの[l,r)を対象とする
+	int Find_Rightmost(int a, int b, T x, int k = 0, int l = 0, int r = -1)
+	{
+		// r=-1 なら最初の呼び出し
+		if(r < 0) r = n;  // [0,n)を対象とする
+
+		// 自分の値がxより大きい   もしくは
+		// クエリ[a,b)と対象[l,r)が交わらない
+		if(node[k] > x || (r <= a || b <= l)) return a-1;  // 自身の右隣を返す
+
+//		if(k >= n-1) return k-(n-1);  // 自分が葉なら、その位置を返す
+		if(k >= n-1) return l;  // 自分が葉なら、その位置を返す
+		// 葉なので、lが位置を表している
+
+		int vr = Find_Rightmost(a, b, x, 2*k+2, (l+r)/2, r);  // 右側
+		if(vr != a-1)  // 右側に答がある
+		{
+			return vr;
+		}
+		else
+		{
+			return Find_Rightmost(a, b, x, 2*k+1, l, (l+r)/2);  // 左側
+		}
+	}
+
+	// 要素xをvalで更新する
+	// Update()と違い、木全体の更新は行わない。Build()の呼び出しが必要。
+	// 用途：初期化時に全要素を設定し、Build()で木を構築する
+	void Set(int i, T val)
+	{
+		i += n-1;  // node[]の要素番号に変換
+		node[i] = val;
+	}
+
+	// 初期化時にSet()で全要素を設定した後、Build()にて木を構築する
+	void Build(void)
+	{
+		// 最下段を除いた右下の要素から、左上方向へ構築していく
+		int i;
+		for(i = n-2; i >= 0; i--)
+		{
+			node[i] = fx(node[2*i+1], node[2*i+2]);
+		}
+	}
+
+	// 要素iを取得する
+	T Get(int i)
+	{
+		i += n-1;  // node[]の要素番号に変換
+		return node[i];
+	}
+};
+
+int main(void)
+{
+	// '('を+1, ')'を-1とする
+	using T = array<int,2>;  // [0]:()の和, [1]:()を左から加算していったときの最小値
+	auto fx = [](T x1, T x2) -> T
+	{
+		T ret;
+		ret[0] = x1[0] + x2[0];
+		ret[1] = min(x1[1], x1[0] + x2[1]);
+		return ret;
+	};
+	T ex = {0, INF32};
+	int N, Q; cin >> N >> Q;
+	SegmentTree<T> seg(N+5, fx, ex);
+	string s; cin >> s;
+	int i;
+	for(i = 0; i < N; i++)
+	{
+		if(s[i] == '(') seg.Set(i, {1, 1});
+		else seg.Set(i, {-1, -1});
+	}
+	seg.Build();
+
+	while(Q > 0)
+	{
+		int k, l, r; cin >> k >> l >> r;
+		l--; r--;
+		if(k == 1)
+		{
+			auto dl = seg.Get(l);
+			auto dr = seg.Get(r);
+			seg.Update(l, dr);
+			seg.Update(r, dl);
+		}	
+		else
+		{
+			r++;  // 半開区間へ
+			auto d = seg.Query(l, r);
+			YesNo(d[0] == 0 && d[1] >= 0);  // 総和が0で、途中で負にならないならOK
+		}
+		Q--;
+	}
+
+	return 0;
+}
