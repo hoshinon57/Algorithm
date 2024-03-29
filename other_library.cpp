0bf2e8bfd5ac713c185d8968f45a623b35b10886
hoshinon <yuuki.hoshino.76@gmail.com>
2024-03-29 21:34:06 +0900

    SCC追加、増えてきたのでファイル先頭に概要を列挙

--- other_library.cpp
+++ other_library.cpp
@@ -8,7 +8,12 @@ typedef long long ll;
 
 /*
  * その他のライブラリ
- * 
+ * ・床関数,天井関数
+ * ・bビット目の確認、ビットON/OFF
+ * ・N*N配列を回転
+ * ・nをbase進法で表したときの値
+ * ・extgcd
+ * ・SCC
  */
 
 // a/b以下の最大の整数(床関数) floor(5,2)=2, floor(-5,2)=-3
@@ -71,6 +76,142 @@ long long extgcd(long long a, long long b, long long &x, long long &y) {
 	long long d = extgcd(b, a%b, y, x);
 	y -= a/b * x;
 	return d;
+}
+
+/*
+ * SCC(Strongly Connected Components / 強連結成分分解)のライブラリ
+ * [SCCとは]
+ *   有向グラフにて、互いに行き来できる全頂点を過不足なくグループ化したもの。
+ *   強連結成分分解したグループを1頂点とみると、DAG(有向非巡回グラフ)となる。
+ * 
+ * [使い方]
+ *   Graph = vector<vector<int>>; 型で有向グラフを構築する。
+ *   SCC scc;
+ *   vector<vector<int>> grp;
+ *   vector<int> idx;
+ *   scc.build(g, grp, idx);
+ *   にて強連結成分分解を行う。 ※grp,idxの初期化は不要
+ *   grp[i][*]:グループiに属する頂点一覧
+ *   idx[i]:頂点iのグループ番号
+ *   が保存される。
+ * 
+ *   Test_AOJ_GRL_3_C()も参照のこと。
+ * 
+ * [参考資料]
+ *   https://manabitimes.jp/math/1250
+ *   https://ei1333.github.io/luzhiled/snippets/graph/strongly-connected-components.html
+ *   https://scrapbox.io/pocala-kyopro/%E5%BC%B7%E9%80%A3%E7%B5%90%E6%88%90%E5%88%86%E5%88%86%E8%A7%A3
+ *   https://drken1215.hatenablog.com/entry/2023/05/06/133900
+ * 
+ * [関連する問題 / verifyした問題]
+ *   AOJ GRL_3_C https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_C&lang=jp
+ */
+
+using Graph = vector<vector<int>>;
+struct SCC {
+private:
+	vector<bool> seen;
+	vector<int> order;
+	// 通常の方向にDFS
+	void dfs(Graph &g, int v)
+	{
+		if(seen[v]) return;
+		seen[v] = true;
+		for(auto &e : g[v]) dfs(g, e);
+		order.push_back(v);  // 帰りがけ順に記録
+	}
+	// 逆方向にDFS
+	void rdfs(Graph &rg, vector<int> &idx, int v, int gr)
+	{
+		if(idx[v] != -1) return;
+		idx[v] = gr;
+		for(auto &e : rg[v]) rdfs(rg, idx, e, gr);
+	}
+public:
+	// 有向グラフgを強連結成分に分解し、結果をgrp,idxに保存する
+	// grp[i][*]:グループiに属する頂点一覧
+	// idx[i]:頂点iのグループ番号
+	// grp,idxともに、関数内でまず初期化される
+	void build(Graph &g, vector<vector<int>> &grp, vector<int> &idx)
+	{
+		int i;
+		int gsz = g.size();
+		// 逆方向のグラフを構築
+		Graph rg(gsz);
+		for(i = 0; i < gsz; i++)
+		{
+			// i->eの辺に対し、e->iの辺を張る
+			for(auto &e : g[i]) rg[e].push_back(i);
+		}
+
+		// idx構築
+		seen.assign(gsz, false);
+		order.clear();
+		for(i = 0; i < gsz; i++)
+		{
+			dfs(g, i);
+		}
+		reverse(order.begin(), order.end());
+		int gr = 0;  // 強連結成分の数
+		idx.assign(gsz, -1);
+		for(auto &e : order)
+		{
+			if(idx[e] != -1) continue;
+			rdfs(rg, idx, e, gr);
+			gr++;
+		}
+
+		// grp構築
+		grp.clear();
+		grp.resize(gr);
+		for(i = 0; i < gsz; i++)
+		{
+			grp[idx[i]].push_back(i);
+		}
+
+#if 0  // 縮めたグラフを構築する場合、以下のようになる模様 (動作確認はほとんどできていない)
+		Graph shg;  // 呼び出し元から参照で渡す
+		shg.clear();
+		shg.resize(gr);
+		for(i = 0; i < gsz; i++)
+		{
+			for(auto &e : g[i])  // i->e
+			{
+				int x = idx[i];
+				int y = idx[e];
+				if(x != y) shg[x].push_back(y);
+			}
+		}
+#endif
+	}
+};
+
+// SCC使用のサンプルコード
+// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_C&lang=jp
+void Test_AOJ_GRL_3_C(void)
+{
+	int i;
+	int V, E; cin >> V >> E;
+	Graph g(V);
+	for(i = 0; i < E; i++)
+	{
+		int s, t; cin >> s >> t;  // s->t
+		g[s].push_back(t);
+	}
+	SCC scc;
+	vector<vector<int>> grp;
+	vector<int> idx;
+	scc.build(g, grp, idx);
+
+	int Q; cin >> Q;
+	while(Q > 0)
+	{
+		Q--;
+		int u, v; cin >> u >> v;
+		int ans = 0;
+		if(idx[u] == idx[v]) ans = 1;
+		cout << ans << endl;
+	}
 }
 
 int main(void)
