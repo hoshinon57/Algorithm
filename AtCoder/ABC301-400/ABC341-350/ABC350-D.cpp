2b20f494058c1b513d9625c70561473828f69a62
hoshinon <yuuki.hoshino.76@gmail.com>
2024-05-01 11:35:52 +0900

    ABC350 D問題(UnionFind)

---
+++ AtCoder/ABC301-400/ABC341-350/ABC350-D.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+typedef long long ll;
+// const ll INF64 = 1LL << 60;
+const ll INF64 = ((1LL<<62)-(1LL<<31));  // 10^18より大きく、かつ2倍しても負にならない数
+const int INF32 = 0x3FFFFFFF;  // =(2^30)-1 10^9より大きく、かつ2倍しても負にならない数
+template<class T> inline bool chmin(T &a, T b) { if(a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T &a, T b) { if(a < b) { a = b; return true; } return false; }
+#define YesNo(T) cout << ((T) ? "Yes" : "No") << endl;  // T:bool
+
+// ABC350 https://atcoder.jp/contests/abc350
+
+/*
+ * UnionFindで解く。公式解説の通り。
+ *   https://atcoder.jp/contests/abc350/editorial/9811
+ * 
+ * ある連結成分の頂点数が4であれば、最大で 4*(4-1)/2=6本 の辺を結ぶことができる。
+ * 連結成分ごとに上記を加算していき、既に辺が結ばれているM本を引いたものが答。
+ */
+
+struct UnionFind
+{
+	vector<int> parent;  // parent[x]:xの親の番号 xが根であれば-1
+	vector<int> siz;  // siz[x]:xの属するグループの頂点数
+
+	// 頂点数nで初期化
+	UnionFind(int n) : parent(n, -1), siz(n, 1) {}
+
+	// 根を求める
+	int root(int x)
+	{
+		if(parent[x] == -1) return x;  // xが根であれば、xを返す
+		else
+		{
+			parent[x] = root(parent[x]);
+			return parent[x];
+		}
+	}
+
+	// xとyが同じグループに属するか
+	bool issame(int x, int y)
+	{
+		return root(x) == root(y);  // 根が同一なら同じグループ
+	}
+
+	// xを含むグループとyを含むグループを併合する
+	bool unite(int x, int y)
+	{
+		// x,yをそれぞれ根まで移動させる
+		x = root(x);
+		y = root(y);
+
+		// すでに同じグループであれば何もしない
+		if(x == y) return false;
+
+		// 頂点数が少ない方が子になるようにする
+		if(siz[x] < siz[y])
+		{
+			// xをyの子にする
+			parent[x] = y;
+			siz[y] += siz[x];
+		}
+		else
+		{
+			// yをxの子にする
+			parent[y] = x;
+			siz[x] += siz[y];
+		}
+		return true;
+	}
+
+	// xを含むグループの頂点数
+	int size(int x)
+	{
+		return siz[root(x)];
+	}
+};
+
+int main(void)
+{
+	int i;
+	ll N, M; cin >> N >> M;
+	UnionFind uf(N);
+	for(i = 0; i < M; i++)
+	{
+		int a, b; cin >> a >> b;
+		a--; b--;
+		uf.unite(a, b);
+	}
+
+	ll ans = 0;
+	for(i = 0; i < N; i++)
+	{
+		if(i != uf.root(i)) continue;  // 連結成分ごとに計算
+		ll sz = uf.size(i);
+		ans += sz*(sz-1)/2;
+	}
+	ans -= M;
+	cout << ans << endl;
+
+	return 0;
+}
