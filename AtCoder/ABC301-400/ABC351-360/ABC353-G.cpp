57afe50c0c0dbf7108978c64bc2c58edfdefdf7d
hoshinon <yuuki.hoshino.76@gmail.com>
2024-05-15 22:56:35 +0900

    ABC353 G問題(セグメント木) 自力で解けず、解説を見た

---
+++ AtCoder/ABC301-400/ABC351-360/ABC353-G.cpp
@@ -0,0 +1,220 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <functional>  // function
+#include <limits>  // numeric_limits
+using namespace std;
+typedef long long ll;
+// const ll INF64 = 1LL << 60;
+const ll INF64 = ((1LL<<62)-(1LL<<31));  // 10^18より大きく、かつ2倍しても負にならない数
+const int INF32 = 0x3FFFFFFF;  // =(2^30)-1 10^9より大きく、かつ2倍しても負にならない数
+template<class T> inline bool chmin(T &a, T b) { if(a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T &a, T b) { if(a < b) { a = b; return true; } return false; }
+#define YesNo(T) cout << ((T) ? "Yes" : "No") << endl;  // T:bool
+
+// ABC353 https://atcoder.jp/contests/abc353
+
+/*
+ * 自力で解けず、解説を見た。セグメント木で解く。
+ *   https://twitter.com/kyopro_friends/status/1789293137573343656
+ *   https://atcoder.jp/contests/abc353/editorial/9953
+ *   https://www.youtube.com/watch?v=u6nTE12iIGY
+ * 
+ * dp[i]を「最後にいる市場がiのときのX」と定義すると、
+ * 市場iへの移動は、全市場j(1<=j<=N)からの移動を考えて以下となる。
+ *   dp[i] = max(dp[j] - C|i-j| + Pi)
+ * 場合分けで絶対値を外すと
+ *   max(dp[j]+C*j - C*i + Pi) = max(dp[j]+C*j) - C*i + Pi  (i>j)
+ *   max(dp[j]-C*j + C*i + Pi) = max(dp[j]-C*j) + C*i + Pi  (i<=j)
+ * となる。
+ * dp[j]±C*jの部分が町ごとの値であり、これを2本のセグメント木で管理すれば、
+ * dp[i]はクエリごとO(logN)で求まる。
+ * 
+ * クエリごと求めたdp[i]について、セグ木には±C*jしたものをUpdateする必要がある。
+ * 
+ * 町1から順にポテンシャルがCずつ減っていくケースと、Cずつ増えていくケースがあるイメージ。
+ * 
+ * [どう思考すれば解法にたどり着けるか]
+ * ・DPをセグメント木で高速化、っぽいところまでは思いついた。
+ *   そこから先、セグ木を2本持たせる部分が思いつかなかった。
+ * ・＞絶対値が入ってるのは場合分けで絶対値を外すのが鉄則
+ *   であり、まずは立式を頑張るべきだったか。
+ */
+
+template <typename T>
+struct SegmentTree
+{
+private:
+	using FX = function<T(T, T)>;  // X*X->X となる関数の型
+	int n;   // 木の最下段の要素数 (コンストラクタで指定したsize以上の、2のべき乗)
+	FX fx;  // モノイドX上での二項演算
+	const T ex;  // モノイドX上の単位元
+	vector<T> node;
+
+public:
+	// 要素数で初期化
+	SegmentTree(int size, FX fx_, T ex_) : fx(fx_), ex(ex_)
+	{
+		// 最下段のノード数は、size以上の2のべき乗 -> nとする
+		// するとセグメント木全体で必要なノード数は 2*n-1 となる
+		n = 1;
+		while(n < size) n *= 2;
+		node.resize(2*n-1, ex);  // 単位元で初期化
+	}
+
+	// 要素xをvalで更新する
+	void Update(int x, T val)
+	{
+		x += n-1;  // node[]の要素番号に変換
+		// 最下段のnodeを更新したら、あとは親へ上って更新していく
+		node[x] = val;
+		while(x > 0)
+		{
+			x = (x-1)/2;  // 親
+			node[x] = fx(node[2*x+1], node[2*x+2]);
+		}
+	}
+
+	// 区間[a,b)にある要素のモノイド積を返す
+	// k:自分がいるnodeのindex
+	// nodeの[l,r)を対象とする
+	T Query(int a, int b, int k = 0, int l = 0, int r = -1)
+	{
+		// r=-1 なら最初の呼び出し
+		if(r < 0) r = n;  // [0,n)を対象とする
+
+		// クエリ[a,b)と対象[l,r)が交わらないので、答に影響しない値(=単位元)を返す
+		if(r <= a || b <= l) return ex;
+
+		// クエリが対象を完全に被覆する
+		if(a <= l && r <= b) return node[k];
+
+		// 左右の子について再帰的に探索
+		T vl = Query(a, b, 2*k+1, l, (l+r)/2);  // 左側
+		T vr = Query(a, b, 2*k+2, (l+r)/2, r);  // 右側
+		return fx(vl, vr);
+	}
+
+	// [注意]
+	// 以下のFind_Leftmostは、x以下のものを探すのであれば(現状のソースコード)、Range Minのfx,exを指定すること。
+	// x以上のものを探したい場合、
+	//   Range "MAX"のfx,exを指定
+	//   関数内を1か所修正：node[k] > x を node[k] < x へ変更
+	// とすること。
+
+	// [a,b)の範囲で、x以下となる最も左側の要素番号を返す
+	// 範囲内にx以下が見つからなければ、b(=範囲外)を返す
+	// k:自分がいるnodeのindex
+	// nodeの[l,r)を対象とする
+	int Find_Leftmost(int a, int b, T x, int k = 0, int l = 0, int r = -1)
+	{
+		// r=-1 なら最初の呼び出し
+		if(r < 0) r = n;  // [0,n)を対象とする
+
+		// 自分の値がxより大きい   もしくは
+		// クエリ[a,b)と対象[l,r)が交わらない
+		if(node[k] > x || (r <= a || b <= l)) return b;  // 自身の右隣を返す
+
+//		if(k >= n-1) return k-(n-1);  // 自分が葉なら、その位置を返す
+		if(k >= n-1) return l;  // 自分が葉なら、その位置を返す
+		// 葉なので、lが位置を表している
+
+		int vl = Find_Leftmost(a, b, x, 2*k+1, l, (l+r)/2);  // 左側
+		if(vl != b)  // 左側に答がある
+		{
+			return vl;
+		}
+		else
+		{
+			return Find_Leftmost(a, b, x, 2*k+2, (l+r)/2, r);  // 右側
+		}
+	}
+
+	// [注意]Find_Leftmost()側のコメント参照
+
+	// [a,b)の範囲で、x以下となる最も右側の要素番号を返す
+	// 範囲内にx以下が見つからなければ、a-1(=範囲外)を返す
+	// k:自分がいるnodeのindex
+	// nodeの[l,r)を対象とする
+	int Find_Rightmost(int a, int b, T x, int k = 0, int l = 0, int r = -1)
+	{
+		// r=-1 なら最初の呼び出し
+		if(r < 0) r = n;  // [0,n)を対象とする
+
+		// 自分の値がxより大きい   もしくは
+		// クエリ[a,b)と対象[l,r)が交わらない
+		if(node[k] > x || (r <= a || b <= l)) return a-1;  // 自身の右隣を返す
+
+//		if(k >= n-1) return k-(n-1);  // 自分が葉なら、その位置を返す
+		if(k >= n-1) return l;  // 自分が葉なら、その位置を返す
+		// 葉なので、lが位置を表している
+
+		int vr = Find_Rightmost(a, b, x, 2*k+2, (l+r)/2, r);  // 右側
+		if(vr != a-1)  // 右側に答がある
+		{
+			return vr;
+		}
+		else
+		{
+			return Find_Rightmost(a, b, x, 2*k+1, l, (l+r)/2);  // 左側
+		}
+	}
+
+	// 要素xをvalで更新する
+	// Update()と違い、木全体の更新は行わない。Build()の呼び出しが必要。
+	// 用途：初期化時に全要素を設定し、Build()で木を構築する
+	void Set(int i, T val)
+	{
+		i += n-1;  // node[]の要素番号に変換
+		node[i] = val;
+	}
+
+	// 初期化時にSet()で全要素を設定した後、Build()にて木を構築する
+	void Build(void)
+	{
+		// 最下段を除いた右下の要素から、左上方向へ構築していく
+		int i;
+		for(i = n-2; i >= 0; i--)
+		{
+			node[i] = fx(node[2*i+1], node[2*i+2]);
+		}
+	}
+
+	// 要素iを取得する
+	T Get(int i)
+	{
+		i += n-1;  // node[]の要素番号に変換
+		return node[i];
+	}
+};
+
+int main(void)
+{
+	// 0-indexed  // [0,N)
+	int i;
+	using T = ll;
+	auto fx = [](T x1, T x2) -> T { return max(x1, x2); };
+	T ex = numeric_limits<T>::min();
+	ll N, C, M; cin >> N >> C >> M;
+	SegmentTree<T> seg1(N+5, fx, ex), seg2(N+5, fx, ex);
+	seg1.Update(0, 0);
+	seg2.Update(0, 0);
+
+	ll ans = 0;
+	for(i = 0; i < M; i++)
+	{
+		ll t, p; cin >> t >> p;
+		t--;
+		ll tmp1 = seg1.Query(0, t) - C*t + p;  // t> jであるj [0,t)
+		ll tmp2 = seg2.Query(t, N) + C*t + p;  // t<=jであるj [t,N)
+		ll d = max(tmp1, tmp2);  // いわゆるdp[i], 市場iが最後にいるときのX
+		chmax(ans, d);
+		seg1.Update(t, d + C*t);
+		seg2.Update(t, d - C*t);
+	}
+	cout << ans << endl;
+
+	return 0;
+}
