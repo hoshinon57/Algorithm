9d67f7f052a6313e1f52b5c924a1a23380cd3884
hoshinon <yuuki.hoshino.76@gmail.com>
2024-08-06 23:35:39 +0900

    重み付きUnionFindを追加

--- union_find.cpp
+++ union_find.cpp
@@ -100,6 +100,97 @@ struct UnionFind
 	}
 };
 
+// [verify]ABC328-F
+// 重み付きUnionFind
+// けんちょん氏の実装をほぼそのままお借りしている
+// https://drken1215.hatenablog.com/entry/2023/11/12/040700
+// 参考：https://qiita.com/drken/items/cce6fc5c579051e64fab
+// T:重みの型 int or ll  基本はllが安全と思われる
+// 主に使うメソッド: root(), issame(), unite(), size(), diff()  diff以外は通常のUnionFindと同じ
+template<class T> struct UnionFind_Weighted
+{
+	vector<int> par;
+	vector<T> weight;
+
+	// UnionFind_Weighted() { }
+
+	// 頂点数Nで初期化
+	UnionFind_Weighted(int N, T zero = 0) : par(N, -1), weight(N, zero) {}
+	void init(int N, T zero = 0) {
+		par.assign(N, -1);
+		weight.assign(N, zero);
+	}
+	
+	// 根を求める
+	int root(int x) {
+		if (par[x] < 0) return x;
+		else {
+			int r = root(par[x]);
+			weight[x] += weight[par[x]];
+			return par[x] = r;
+		}
+	}
+
+	// xとyが同じグループに属するか
+	bool issame(int x, int y) {
+		return root(x) == root(y);
+	}
+
+	// xを含むグループの頂点数
+	int size(int x) {
+		return -par[root(x)];
+	}
+	
+	// v[y] - v[x] = w となるように、
+	// xを含むグループとyを含むグループを併合する
+	// x,yが併合済みの場合はfalseを返す (重み付きUnionFindとしての未定義動作)
+	bool unite(int x, int y, T w) {
+		w += get_weight(x), w -= get_weight(y);
+		x = root(x), y = root(y);
+		if (x == y) return false;
+		if (par[x] > par[y]) swap(x, y), w = -w; // merge technique
+		par[x] += par[y];
+		par[y] = x;
+		weight[y] = w;
+		return true;
+	}
+	
+	// get v[x]
+	T get_weight(int x) {
+		root(x);
+		return weight[x];
+	}
+	
+	// xとyが同じグループにいるとき、weight(y) - weight(x) を返す
+	T diff(int x, int y) {
+		return get_weight(y) - get_weight(x);
+	}
+	
+	// get groups
+	vector<vector<int>> groups() {
+		vector<vector<int>> member(par.size());
+		for (int v = 0; v < (int)par.size(); ++v) {
+			member[root(v)].push_back(v);
+		}
+		vector<vector<int>> res;
+		for (int v = 0; v < (int)par.size(); ++v) {
+			if (!member[v].empty()) res.push_back(member[v]);
+		}
+		return res;
+	}
+	
+	// debug
+	friend ostream& operator << (ostream &s, UnionFind_Weighted uf) {
+		const vector<vector<int>> &gs = uf.groups();
+		for (const vector<int> &g : gs) {
+			s << "group: ";
+			for (int v : g) s << v << "(" << uf.get_weight(v) << ") ";
+			s << endl;
+		}
+		return s;
+	}
+};
+
 int main(void)
 {
 	UnionFind uf(7);  // 0～6がそれぞれ異なるグループ
@@ -112,6 +203,16 @@ int main(void)
 
 	uf.unite(1,5);  // {0}, {1,2,3,5,6}, {4}
 	assert(uf.issame(1,3));  // 今度はtrue
+
+	UnionFind_Weighted<int> ufw(10);  // 重みはint型で定義
+	ufw.unite(0, 1, 2);
+	ufw.unite(9, 1, 5);
+	assert( ufw.issame(0, 9));
+    assert(!ufw.issame(2, 9));
+	assert(ufw.diff(0, 9) == -3);
+    assert(ufw.size(0) == 3);
+    assert(ufw.size(9) == 3);
+    assert(ufw.size(2) == 1);
 
 	return 0;
 }
