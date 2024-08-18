0df183928e2eae15938a30e867b8aaeb3940ffb0
hoshinon <yuuki.hoshino.76@gmail.com>
2024-08-18 18:38:50 +0900

    ABC367 F問題

---
+++ AtCoder/ABC301-400/ABC361-370/ABC367-F-2.cpp
@@ -0,0 +1,232 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <functional>  // function
+#include <limits>  // numeric_limits
+#include <array>
+using namespace std;
+typedef long long ll;
+// const ll INF64 = 1LL << 60;
+const ll INF64 = ((1LL<<62)-(1LL<<31));  // 10^18より大きく、かつ2倍しても負にならない数
+const int INF32 = 0x3FFFFFFF;  // =(2^30)-1 10^9より大きく、かつ2倍しても負にならない数
+template<class T> inline bool chmin(T &a, T b) { if(a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T &a, T b) { if(a < b) { a = b; return true; } return false; }
+#define YesNo(T) cout << ((T) ? "Yes" : "No") << endl;  // T:bool
+
+// ABC367 https://atcoder.jp/contests/abc367
+
+/*
+ * コンテスト時に考えた方針。(ACできなかったが)
+ * セグメント木に "A,A^2,A^3" の3要素を持たせ、要素同士の結合は加算とする。
+ * ※加算は適当に大きな値でMODを取る。加算なのでMODは不要かもしれないが。
+ * 
+ * すると各クエリついて、A側とB側の多重集合が
+ * ・等しければ、Query()の計算結果は確実に一致する。
+ * ・不一致であれば、Query()の計算結果は高確率で不一致となる。
+ * となるので、これで判定ができる。
+ * 
+ * コンテスト時は2乗,3乗に頭が回らず、和や積など適当に組み合わせており、ハッシュ衝突があったのかWAだった。
+ * 公式解説のYouTube配信にて2乗3乗のコメントがあり、それを参考に実装したのが本ソースコード。
+ *   https://www.youtube.com/live/w0CgojRHMEo
+ */
+
+// (1)Update(x, val) : 要素xをvalで更新する
+// (2)Query(a, b) : 区間[a,b)にある要素のモノイド積を返す
+// (3)Set(i, val) & Build() : 木の更新を行わず要素iを更新し(Set), まとめて木の構築を行う(Build)
+// (4)Get(i) : 要素iを取得する
+// (5)Find_Leftmost(a, b, x) : 区間[a,b)の範囲で、x以下となる最も左側の要素番号を返す
+// [注意]
+//   0-indexed, および半開区間で処理する。
+// 以下URLをほぼそのまま持ってきている
+// https://algo-logic.info/segment-tree/
+// https://tsutaj.hatenablog.com/entry/2017/03/29/204841
+template <typename T>
+struct SegmentTree
+{
+private:
+	using FX = function<T(T, T)>;  // X*X->X となる関数の型
+	int n;   // 木の最下段の要素数 (コンストラクタで指定したsize以上の、2のべき乗)
+	FX fx;  // モノイドX上での二項演算
+	const T ex;  // モノイドX上の単位元
+	vector<T> node;
+
+public:
+	// 要素数で初期化
+	SegmentTree(int size, FX fx_, T ex_) : fx(fx_), ex(ex_)
+	{
+		// 最下段のノード数は、size以上の2のべき乗 -> nとする
+		// するとセグメント木全体で必要なノード数は 2*n-1 となる
+		n = 1;
+		while(n < size) n *= 2;
+		node.resize(2*n-1, ex);  // 単位元で初期化
+	}
+
+	// 要素xをvalで更新する
+	void Update(int x, T val)
+	{
+		x += n-1;  // node[]の要素番号に変換
+		// 最下段のnodeを更新したら、あとは親へ上って更新していく
+		node[x] = val;
+		while(x > 0)
+		{
+			x = (x-1)/2;  // 親
+			node[x] = fx(node[2*x+1], node[2*x+2]);
+		}
+	}
+
+	// 区間[a,b)にある要素のモノイド積を返す
+	// k:自分がいるnodeのindex
+	// nodeの[l,r)を対象とする
+	T Query(int a, int b, int k = 0, int l = 0, int r = -1)
+	{
+		// r=-1 なら最初の呼び出し
+		if(r < 0) r = n;  // [0,n)を対象とする
+
+		// クエリ[a,b)と対象[l,r)が交わらないので、答に影響しない値(=単位元)を返す
+		if(r <= a || b <= l) return ex;
+
+		// クエリが対象を完全に被覆する
+		if(a <= l && r <= b) return node[k];
+
+		// 左右の子について再帰的に探索
+		T vl = Query(a, b, 2*k+1, l, (l+r)/2);  // 左側
+		T vr = Query(a, b, 2*k+2, (l+r)/2, r);  // 右側
+		return fx(vl, vr);
+	}
+
+	// [注意]
+	// 以下のFind_Leftmostは、x以下のものを探すのであれば(現状のソースコード)、Range Minのfx,exを指定すること。
+	// x以上のものを探したい場合、
+	//   Range "MAX"のfx,exを指定
+	//   関数内を1か所修正：node[k] > x を node[k] < x へ変更
+	// とすること。
+
+	// [a,b)の範囲で、x以下となる最も左側の要素番号を返す
+	// 範囲内にx以下が見つからなければ、b(=範囲外)を返す
+	// k:自分がいるnodeのindex
+	// nodeの[l,r)を対象とする
+	int Find_Leftmost(int a, int b, T x, int k = 0, int l = 0, int r = -1)
+	{
+		// r=-1 なら最初の呼び出し
+		if(r < 0) r = n;  // [0,n)を対象とする
+
+		// 自分の値がxより大きい   もしくは
+		// クエリ[a,b)と対象[l,r)が交わらない
+		if(node[k] > x || (r <= a || b <= l)) return b;  // 自身の右隣を返す
+
+//		if(k >= n-1) return k-(n-1);  // 自分が葉なら、その位置を返す
+		if(k >= n-1) return l;  // 自分が葉なら、その位置を返す
+		// 葉なので、lが位置を表している
+
+		int vl = Find_Leftmost(a, b, x, 2*k+1, l, (l+r)/2);  // 左側
+		if(vl != b)  // 左側に答がある
+		{
+			return vl;
+		}
+		else
+		{
+			return Find_Leftmost(a, b, x, 2*k+2, (l+r)/2, r);  // 右側
+		}
+	}
+
+	// [注意]Find_Leftmost()側のコメント参照
+
+	// [a,b)の範囲で、x以下となる最も右側の要素番号を返す
+	// 範囲内にx以下が見つからなければ、a-1(=範囲外)を返す
+	// k:自分がいるnodeのindex
+	// nodeの[l,r)を対象とする
+	int Find_Rightmost(int a, int b, T x, int k = 0, int l = 0, int r = -1)
+	{
+		// r=-1 なら最初の呼び出し
+		if(r < 0) r = n;  // [0,n)を対象とする
+
+		// 自分の値がxより大きい   もしくは
+		// クエリ[a,b)と対象[l,r)が交わらない
+		if(node[k] > x || (r <= a || b <= l)) return a-1;  // 自身の右隣を返す
+
+//		if(k >= n-1) return k-(n-1);  // 自分が葉なら、その位置を返す
+		if(k >= n-1) return l;  // 自分が葉なら、その位置を返す
+		// 葉なので、lが位置を表している
+
+		int vr = Find_Rightmost(a, b, x, 2*k+2, (l+r)/2, r);  // 右側
+		if(vr != a-1)  // 右側に答がある
+		{
+			return vr;
+		}
+		else
+		{
+			return Find_Rightmost(a, b, x, 2*k+1, l, (l+r)/2);  // 左側
+		}
+	}
+
+	// 要素xをvalで更新する
+	// Update()と違い、木全体の更新は行わない。Build()の呼び出しが必要。
+	// 用途：初期化時に全要素を設定し、Build()で木を構築する
+	void Set(int i, T val)
+	{
+		i += n-1;  // node[]の要素番号に変換
+		node[i] = val;
+	}
+
+	// 初期化時にSet()で全要素を設定した後、Build()にて木を構築する
+	void Build(void)
+	{
+		// 最下段を除いた右下の要素から、左上方向へ構築していく
+		int i;
+		for(i = n-2; i >= 0; i--)
+		{
+			node[i] = fx(node[2*i+1], node[2*i+2]);
+		}
+	}
+
+	// 要素iを取得する
+	T Get(int i)
+	{
+		i += n-1;  // node[]の要素番号に変換
+		return node[i];
+	}
+};
+
+int main(void)
+{
+	const ll MOD = INF64;
+	using T = array<ll,3>;
+	auto fx = [](T x1, T x2) -> T
+	{
+		T ret;
+		ret[0] = (x1[0] + x2[0]) % MOD;
+		ret[1] = (x1[1] + x2[1]) % MOD;
+		ret[2] = (x1[2] + x2[2]) % MOD;
+		return ret;
+	};
+	T ex = {0, 0, 0};
+	// -----
+	int i;
+	ll N, Q; cin >> N >> Q;
+	SegmentTree<T> segA(N+5, fx, ex), segB(N+5, fx, ex);
+	for(i = 0; i < N; i++)
+	{
+		ll a; cin >> a;
+		segA.Set(i, {a, a*a, a*a*a});
+	}
+	segA.Build();
+	for(i = 0; i < N; i++)
+	{
+		ll b; cin >> b;
+		segB.Set(i, {b, b*b, b*b*b});
+	}
+	segB.Build();
+
+	while(Q > 0)
+	{
+		Q--;
+		ll l1, r1, l2, r2; cin >> l1 >> r1 >> l2 >> r2;
+		l1--; r1--; l2--; r2--;
+		r1++; r2++;  // [l,r)
+		YesNo(segA.Query(l1, r1) == segB.Query(l2, r2));
+	}
+
+	return 0;
+}
