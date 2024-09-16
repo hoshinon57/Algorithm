ee1d90d8431e8d9124f29d36f8bc5ba6da61734a
hoshinon <yuuki.hoshino.76@gmail.com>
2024-09-16 22:34:51 +0900

    主に抽象化を実施
    その他、先頭のmemo/todoをメモ更新

--- dp-rerooting.cpp
+++ dp-rerooting.cpp
@@ -17,10 +17,16 @@ template<class T> inline bool chmax(T &a, T b) { if(a < b) { a = b; return true;
 // ★注意★ #include <functional> を忘れずに。ローカル環境では無くてもビルドが通るが、AtCoderではCEになる。
 
 // memo/todo:
+// dfs1,dfs2をコメント等整理
+// [済]抽象化 add_edge
 // [済]木の直径を解いてみる
 // EDPC-V
 // [済]ABC222-F
-// 2024/6ごろABC-Eの、直径で解く問題を全方位木DPで
+// 2024/6ごろABC-Eの、直径で解く問題を全方位木DPで -> これはいいか
+// ABC348-E
+// ABC220-F(Distance Sums 2)
+// ABC160-F(Distributing Integers) 挑戦
+// TDPC-N(木) 挑戦 https://atcoder.jp/contests/tdpc/tasks/tdpc_tree
 
 /*
  * [ざっくり概要]
@@ -51,7 +57,11 @@ template<class T> inline bool chmax(T &a, T b) { if(a < b) { a = b; return true;
  * ・典型90-003 https://atcoder.jp/contests/typical90/tasks/typical90_c 木の直径
  */
 
-vector<ll> d;  // ABC222-F用
+// 問題ごとに必要なら書き換え
+struct Edge {
+	int to;
+	ll w;  // 辺の重み(重み無しは0)
+};
 
 // 全方位木DPのライブラリ
 // [事前準備]
@@ -66,7 +76,7 @@ vector<ll> d;  // ABC222-F用
 // https://algo-logic.info/tree-dp/
 template <typename T>
 struct Rerooting {
-#if 1  // 問題ごとに書き換え
+#if 0  // 問題ごとに書き換え
 	function<T(T,T)> merge = [](T x1, T x2) -> T {
 		return T(max(x1, x2));
 	};
@@ -90,7 +100,7 @@ struct Rerooting {
 		int to;
 		ll w;  // 辺の重み(重み無しは0)
 	};
-#else  // 各頂点について、その頂点を根としたときに(自身を含め)頂点がいくつあるかを求める場合 もちろん答えは全てN.
+#elif 0  // 各頂点について、その頂点を根としたときに(自身を含め)頂点がいくつあるかを求める場合 もちろん答えは全てN.
 	const T identity = 0;
 	function<T(T,T)> merge = [](T x1, T x2) -> T {
 		return T(x1 + x2);
@@ -105,12 +115,19 @@ struct Rerooting {
 	const T identity;
 	using Graph = vector<vector<Edge>>;
 
+	using f1 = function<T(T,T)>; // merge
+	using f2 = function<T(T,Edge)>;  // add_edge(T,e):DP値に頂点root->childへの辺を加える
+	using f3 = function<T(T,int)>;  // add_root(DP,v):DP値に根(頂点v)を加える
+	f1 merge;
+	f2 add_edge;
+	f3 add_root;
+
 	vector<vector<T>> dp;  // dp[v][i]:頂点vを根として考えたときに、i番目の有効辺に対応する値
 	vector<T> ans;  // ans[v]:頂点vに対する答
 	Graph g;
 
 	// 頂点数N,単位元idntで初期化
-	Rerooting(int N, T idnt) : identity(idnt) {
+	Rerooting(int N, T idnt, f1 merge_, f2 add_edge_, f3 add_root_) : identity(idnt), merge(merge_), add_edge(add_edge_), add_root(add_root_) {
 		g.resize(N);
 		dp.resize(N);
 		ans.resize(N, identity);
@@ -128,7 +145,8 @@ struct Rerooting {
 			if(u == p) continue;
 			dp[v][i] = dfs1(u, v);
 			// test make_edge
-			dp[v][i] += g[v][i].w;
+			dp[v][i] = add_edge(dp[v][i], g[v][i]);
+			// dp[v][i] += g[v][i].w;
 			dpcum = merge(dpcum, dp[v][i]);
 		}
 		return add_root(dpcum, v);
@@ -159,7 +177,8 @@ struct Rerooting {
 //			dfs2(u, add_root(tmp, v), v);
 			tmp = add_root(tmp, v);
 			// test make_edge
-			tmp += g[v][i].w;
+			tmp = add_edge(tmp, g[v][i]);
+			// tmp += g[v][i].w;
 			dfs2(u, tmp, v);
 		}
 	}
@@ -172,28 +191,22 @@ struct Rerooting {
 // https://atcoder.jp/contests/typical90/tasks/typical90_c
 void Test_Tenkei90_003(void)
 {
-/*	
-	(1)merge,add_root,Edgeは以下を設定	
-	(2)dfs2()にてans[v]へは以下を設定
-	   ans[v] = add_root(val_l[deg]);
-	-------
-	function<T(T,T)> merge = [](T x1, T x2) -> T {
-		return T(max(x1, x2));
-	};
-	function<T(T)> add_root = [](T x) -> T {
-		return T(x + 1);
-	};
-	struct Edge {
-		int to;
-		ll w;  // 辺の重み(重み無しは0)
-	};
-	-------
-*/
 	int i;
 	using T = int;
+	auto merge = [](T x1, T x2) -> T {
+		return T(max(x1, x2));
+	};
+	auto add_edge = [](T x, Edge e) -> T {
+		return x;  // 辺の重み無し
+	};
+	auto add_root = [](T x, int v) -> T {
+		return T(x + 1);
+	};
+	// Rerooting::dfs2()におけるans[]は以下を設定する
+	// ans[v] = val_l[deg];
 	T identity = 0;
 	int N; cin >> N;
-	Rerooting<T> reroot(N, identity);
+	Rerooting<T> reroot(N, identity, merge, add_edge, add_root);
 	for(i = 0; i < N-1; i++)
 	{
 		int a, b; cin >> a >> b;
@@ -202,16 +215,29 @@ void Test_Tenkei90_003(void)
 		reroot.make_edge(b, {a, 1});
 	}
 	reroot.build();
-	cout << *max_element(reroot.ans.begin(), reroot.ans.end()) << endl;
+	cout << *max_element(reroot.ans.begin(), reroot.ans.end())+1 << endl;
 }
 
 void Test_ABC222_F_Expensive_Expense(void)
 {
 	int i;
 	int N; cin >> N;
+	vector<ll> d(N);
+
 	using T = ll;
+	auto merge = [](T x1, T x2) -> T {
+		return T(max(x1, x2));
+	};
+	auto add_edge = [](T x, Edge e) -> T {
+		return x+e.w;
+	};
+	auto add_root = [&](T x, int v) -> T {
+		return max(x, T(d[v]));
+	};
+	// Rerooting::dfs2()におけるans[]は以下を設定する
+	// ans[v] = val_l[deg];
 	T identity = 0;
-	Rerooting<T> reroot(N, identity);
+	Rerooting<T> reroot(N, identity, merge, add_edge, add_root);
 	for(i = 0; i < N-1; i++)
 	{
 		int a, b;
@@ -221,7 +247,6 @@ void Test_ABC222_F_Expensive_Expense(void)
 		reroot.make_edge(a, {b, c});
 		reroot.make_edge(b, {a, c});
 	}
-	d.resize(N);
 	for(i = 0; i < N; i++) {cin >> d[i];}
 	reroot.build();
 	for(i = 0; i < N; i++)
@@ -308,9 +333,18 @@ int main(void)
 	{
 		int i;
 		using T = int;
+		auto merge = [](T x1, T x2) -> T {
+			return T(max(x1, x2));
+		};
+		auto add_edge = [](T x, Edge e) -> T {
+			return x+e.w;
+		};
+		auto add_root = [&](T x, int v) -> T {
+			return x;  // 頂点の重み無し
+		};
 		T identity = 0;
 		int N; cin >> N;
-		Rerooting<T> reroot(N, identity);
+		Rerooting<T> reroot(N, identity, merge, add_edge, add_root);
 		for(i = 0; i < N-1; i++)
 		{
 			int u, v, edge_w; cin >> u >> v >> edge_w;
