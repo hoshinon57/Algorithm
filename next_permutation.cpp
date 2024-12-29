e366915743b0e6aaeb30d358606c2f2c79c67d43
hoshinon <yuuki.hoshino.76@gmail.com>
2024-12-29 23:41:24 +0900

    next_permutaionの自前実装

--- next_permutation.cpp
+++ next_permutation.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 // next_permutationを用いた、辞書順での順列生成の練習
+// my_next_permutaion()にてnext_permutaion()の自前実装も実施
 // 参考 https://qiita.com/drken/items/872ebc3a2b5caaa4a0d0  の順列全探索
 //      https://qiita.com/siser/items/a91022071b24952d27d9
 //      https://qiita.com/Nikkely/items/0ddca51b3c0e60afbaab
@@ -65,4 +66,41 @@ int main(void)
 	cout << "permutation num:" << count << endl;
 
 	return 0;
+}
+
+// 参考として、next_permutaion()を自前で実装
+// ★値が重複するケースは正しく動作しない。おそらく2つのwhileにて > を >= に直せば良いと思われる。
+// prevも同様の考え方で実装できる -> ABC276-C.cppを参照
+// https://qiita.com/Nikkely/items/0ddca51b3c0e60afbaab
+// https://atcoder.jp/contests/abc276/editorial/5161
+void my_next_permutaion(void) {
+	int i;
+	int N = 4;
+	vector<int> arr(N); for(i = 0; i < N; i++) {arr[i] = i+1;}
+
+	auto func = [](vector<int> &a, int sz) -> bool {
+		// (1)末尾から手前に見ていって、初めてa[j]>a[j+1]の単調増加が崩れるjを見つける。
+		int j = sz - 2;
+		while(j >= 0 && a[j] > a[j+1]) j--;
+		if(j < 0) return false;  // 末尾まで到達
+
+		// (2)また末尾から手前に見ていって、初めてa[j]<a[x]となるxを見つける。
+		int x = sz - 1;
+		while(a[j] > a[x]) x--;
+
+		// (3)swap(a[j],a[x]).
+		// (4)j+1から末尾までをreverse.
+		swap(a[j], a[x]);
+		reverse(a.begin()+j+1, a.end());
+
+		return true;
+	};
+	do {
+		int sz_ = (int)arr.size();
+		for(i = 0; i < sz_; i++) {
+			cout << arr[i];
+			if(i != sz_-1) cout << " ";
+		}
+		cout << endl;
+	} while (func(arr, N));
 }
