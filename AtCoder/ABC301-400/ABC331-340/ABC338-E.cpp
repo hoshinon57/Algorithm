3bf2cb90aeb8f9846afd2cdafcd5ff945ebb3057
hoshinon <yuuki.hoshino.76@gmail.com>
2024-12-31 11:42:14 +0900

    ABC338 E問題(典型90-17を参考に解いた版)

---
+++ AtCoder/ABC301-400/ABC331-340/ABC338-E.cpp
@@ -0,0 +1,272 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <functional>  // function
+#include <limits>  // numeric_limits
+using namespace std;
+typedef long long ll;
+// const ll INF64 = 1LL << 60;
+const ll INF64 = ((1LL<<62)-(1LL<<31));  // 10^18より大きく、かつ2倍しても負にならない数
+const int INF32 = 0x3FFFFFFF;  // =(2^30)-1 10^9より大きく、かつ2倍しても負にならない数
+template<class T> inline bool chmin(T &a, T b) { if(a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T &a, T b) { if(a < b) { a = b; return true; } return false; }
+#define YesNo(T) cout << ((T) ? "Yes" : "No") << endl;  // T:bool
+
+// ABC338 https://atcoder.jp/contests/abc338
+
+/*
+ * コンテスト時はこれとは別手法で解いた。
+ * 「典型90-17のソースコードを流用できる」というツイートを見たので、そちらを先に解いてから流用してみた。
+ * そのため実装内容はtypical90-17.cppをまず参照のこと。
+ * 
+ * …とはいえ、
+ * ・「Ax,Bxは全て相異なる」という制約
+ *   ⇒セグ木など使わずともスタックで簡単に解ける(コンテスト時の自分の実装や、公式解説)
+ * ・A,Bの上限の制約が典型90と異なる
+ *   ⇒修正箇所が各所にあって大変だった
+ * などから、逆に面倒だった感もある…
+ */
+
+// (1)Update(x, val) : 要素xをvalで更新する
+// (2)Query(a, b) : 区間[a,b)にある要素のモノイド積を返す
+// (3)Set(i, val) & Build() : 木の更新を行わず要素iを更新し(Set), まとめて木の構築を行う(Build)
+// (4)Get(i) : 要素iを取得する
+// (5)Find_Leftmost(a, b, x) : 区間[a,b)の範囲で、x以下となる最も左側の要素番号を返す
+// (6)debug(step, width, l, r) : デバッグ出力
+// [注意]
+//   0-indexed, および半開区間で処理する。
+//   要素に配列を乗せる場合、vectorだとなぜか遅い。arrayか生配列(をstructでラップ)を使うこと。
+// 以下URLをほぼそのまま持ってきている
+// https://algo-logic.info/segment-tree/
+// https://tsutaj.hatenablog.com/entry/2017/03/29/204841
+template <typename T>
+struct SegmentTree
+{
+private:
+	using FX = function<T(T, T)>;  // X*X->X となる関数の型
+	int n;   // 木の最下段の要素数 (コンストラクタで指定したsize以上の、2のべき乗)
+	FX fx;  // モノイドX上での二項演算
+	const T ex;  // モノイドX上の単位元
+	vector<T> node;
+
+public:
+	// 要素数で初期化
+	SegmentTree(int size, FX fx_, T ex_) : fx(fx_), ex(ex_)
+	{
+		// 最下段のノード数は、size以上の2のべき乗 -> nとする
+		// するとセグメント木全体で必要なノード数は 2*n-1 となる
+		n = 1;
+		while(n < size) n *= 2;
+		node.resize(2*n-1, ex);  // 単位元で初期化
+	}
+
+	// 要素xをvalで更新する
+	void Update(int x, T val)
+	{
+		x += n-1;  // node[]の要素番号に変換
+		// 最下段のnodeを更新したら、あとは親へ上って更新していく
+		node[x] = val;
+		while(x > 0)
+		{
+			x = (x-1)/2;  // 親
+			node[x] = fx(node[2*x+1], node[2*x+2]);
+		}
+	}
+
+	// 区間[a,b)にある要素のモノイド積を返す
+	// k:自分がいるnodeのindex
+	// nodeの[l,r)を対象とする
+	T Query(int a, int b, int k = 0, int l = 0, int r = -1)
+	{
+		// r=-1 なら最初の呼び出し
+		if(r < 0) r = n;  // [0,n)を対象とする
+
+		// クエリ[a,b)と対象[l,r)が交わらないので、答に影響しない値(=単位元)を返す
+		if(r <= a || b <= l) return ex;
+
+		// クエリが対象を完全に被覆する
+		if(a <= l && r <= b) return node[k];
+
+		// 左右の子について再帰的に探索
+		T vl = Query(a, b, 2*k+1, l, (l+r)/2);  // 左側
+		T vr = Query(a, b, 2*k+2, (l+r)/2, r);  // 右側
+		return fx(vl, vr);
+	}
+
+	// [注意]
+	// 以下のFind_Leftmostは、x以下のものを探すのであれば(現状のソースコード)、Range Minのfx,exを指定すること。
+	// x以上のものを探したい場合、
+	//   Range "MAX"のfx,exを指定
+	//   関数内を1か所修正：node[k] > x を node[k] < x へ変更
+	// とすること。
+
+	// [a,b)の範囲で、x以下となる最も左側の要素番号を返す
+	// 範囲内にx以下が見つからなければ、b(=範囲外)を返す
+	// k:自分がいるnodeのindex
+	// nodeの[l,r)を対象とする
+	int Find_Leftmost(int a, int b, T x, int k = 0, int l = 0, int r = -1)
+	{
+		// r=-1 なら最初の呼び出し
+		if(r < 0) r = n;  // [0,n)を対象とする
+
+		// 自分の値がxより大きい   もしくは
+		// クエリ[a,b)と対象[l,r)が交わらない
+		if(node[k] > x || (r <= a || b <= l)) return b;  // 自身の右隣を返す
+
+//		if(k >= n-1) return k-(n-1);  // 自分が葉なら、その位置を返す
+		if(k >= n-1) return l;  // 自分が葉なら、その位置を返す
+		// 葉なので、lが位置を表している
+
+		int vl = Find_Leftmost(a, b, x, 2*k+1, l, (l+r)/2);  // 左側
+		if(vl != b)  // 左側に答がある
+		{
+			return vl;
+		}
+		else
+		{
+			return Find_Leftmost(a, b, x, 2*k+2, (l+r)/2, r);  // 右側
+		}
+	}
+
+	// [注意]Find_Leftmost()側のコメント参照
+
+	// [a,b)の範囲で、x以下となる最も右側の要素番号を返す
+	// 範囲内にx以下が見つからなければ、a-1(=範囲外)を返す
+	// k:自分がいるnodeのindex
+	// nodeの[l,r)を対象とする
+	int Find_Rightmost(int a, int b, T x, int k = 0, int l = 0, int r = -1)
+	{
+		// r=-1 なら最初の呼び出し
+		if(r < 0) r = n;  // [0,n)を対象とする
+
+		// 自分の値がxより大きい   もしくは
+		// クエリ[a,b)と対象[l,r)が交わらない
+		if(node[k] > x || (r <= a || b <= l)) return a-1;  // 自身の右隣を返す
+
+//		if(k >= n-1) return k-(n-1);  // 自分が葉なら、その位置を返す
+		if(k >= n-1) return l;  // 自分が葉なら、その位置を返す
+		// 葉なので、lが位置を表している
+
+		int vr = Find_Rightmost(a, b, x, 2*k+2, (l+r)/2, r);  // 右側
+		if(vr != a-1)  // 右側に答がある
+		{
+			return vr;
+		}
+		else
+		{
+			return Find_Rightmost(a, b, x, 2*k+1, l, (l+r)/2);  // 左側
+		}
+	}
+
+	// 要素xをvalで更新する
+	// Update()と違い、木全体の更新は行わない。Build()の呼び出しが必要。
+	// 用途：初期化時に全要素を設定し、Build()で木を構築する
+	void Set(int i, T val)
+	{
+		i += n-1;  // node[]の要素番号に変換
+		node[i] = val;
+	}
+
+	// 初期化時にSet()で全要素を設定した後、Build()にて木を構築する
+	void Build(void)
+	{
+		// 最下段を除いた右下の要素から、左上方向へ構築していく
+		int i;
+		for(i = n-2; i >= 0; i--)
+		{
+			node[i] = fx(node[2*i+1], node[2*i+2]);
+		}
+	}
+
+	// 要素iを取得する
+	T Get(int i)
+	{
+		i += n-1;  // node[]の要素番号に変換
+		return node[i];
+	}
+
+#if 0
+	// デバッグ出力 [i,i+width)でiはstepごと増加、全体は[l,r)でQueryを呼び出していく
+	// debug(1, (好みの値)) とかで呼び出すのが多そうか
+	// 要素の型によってはcoutできないものもあるため、ビルドエラー防止で#if 0としている
+	void debug(int step, int width, int l = -1, int r = -1)
+	{
+		if(l == -1) { l = 0; r = n; }  // 引数無しは全区間
+		cout << "debug:[" << l << "," << r << ")" << endl;
+		for(int i = l; i < r; i += step)
+		{
+			cout << " [" << i << "," << i+width << "):" << this->Query(i, i+width) << endl;
+		}
+	}
+#endif
+};
+
+int main(void)
+{
+	ll i;
+	ll N; cin >> N;
+	ll PM = N*2;  // a,bの値は[0,PM)
+	// l,rは0-indexedに直し、またl<rにしておく
+	vector<ll> l(N), r(N); for(i = 0; i < N; i++) {cin >> l[i] >> r[i]; l[i]--; r[i]--; if(l[i]>r[i]) swap(l[i],r[i]);}
+
+	ll ans = 0;
+	// (1)L1,L2,R1,R2に一致があるケース -> ABC338-Eでは制約により存在しない
+	{
+		vector<ll> cnt(PM);
+		for(i = 0; i < N; i++)
+		{
+			cnt[l[i]]++;
+			cnt[r[i]]++;
+		}
+		for(i = 0; i < PM; i++)
+		{
+			ans += (cnt[i]*(cnt[i]-1)/2);
+		}
+	}
+
+	// (2)R1<L2のケース
+	{
+		vector<ll> imos(PM+5);
+		for(i = 0; i < N; i++)
+		{
+			imos[r[i]]++;
+		}
+		for(i = 0; i < PM+3; i++)
+		{
+			imos[i+1] += imos[i];
+		}
+		for(i = 0; i < N; i++)
+		{
+			if(l[i] == 0) continue;
+			ans += imos[l[i]-1];
+		}
+	}
+	
+	// (3)L1<L2<R2<L2のケース
+	{
+		// Range Sum Query(RSQ)
+		using T = ll;
+		auto fx = [](T x1, T x2) -> T { return x1+x2; };
+		T ex = 0;
+		SegmentTree<T> seg(PM+5, fx, ex);
+		vector<pair<ll,ll>> d;
+		for(i = 0; i < N; i++)
+		{
+			d.push_back({r[i], l[i]});
+		}
+		sort(d.begin(), d.end());  // Rの小さい順、Rが同じならLの小さい順
+		for(i = 0; i < N; i++)
+		{
+			auto [nr,nl] = d[i];
+			ans += seg.Query(nl+1, PM);
+			seg.Update(nl, seg.Get(nl)+1);
+		}
+	}
+	
+	ll tot = N*(N-1)/2;
+	YesNo(tot-ans != 0);
+
+	return 0;
+}
